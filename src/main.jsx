7d29a9b8a4d39c454bb9ec7e9c226368463bc190
fevziye35 <fevziye35@gmail.com>
2026-05-14 16:00:53 +0300

    Fix: Ensure i18n is imported before App

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './i18n'; // ÖNEMLİ: App'ten önce en yukarıda olmalı
+import './index.css';
+import App from './App.jsx';
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import './i18n'; // i18n.js dosyasını projeye dahil eder
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
